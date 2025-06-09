9a318026829629fefbbf35fa97c5b256c587e1e9
Tu Nombre <d.elguezabal@alumnos.upm.es>
2025-06-09 10:43:07 +0200

    se ha hecho la busqueda persistente tras actualizar el navegador

--- src/app/books/pages/search-page/search-bar/search-bar.component.ts
+++ src/app/books/pages/search-page/search-bar/search-bar.component.ts
@@ -6,12 +6,25 @@ import { ChangeDetectionStrategy, Component, EventEmitter, Output, output } from
   templateUrl: './search-bar.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
+
+
 export default class SearchBarComponent {
 
+ searchValue: string = '';
+
+constructor(){
+  const savedQuery = localStorage.getItem('lastSearchQuery');
+  if(savedQuery){
+    this.searchValue = savedQuery;
+  } 
+}
 
   @Output() query = new EventEmitter<string>();
 
+  
+
   onSearchBook(query: string){
+    localStorage.setItem('lastSearchQuery', query);
     this.query.emit(query);
   }
 
